2d8efec7590ef7060b3d8547ee038b9a3f2382bd
Anna Dick <anna_dick@outlook.de>
2023-08-25 14:58:47 +0200

    work on functions

--- src/App.js
+++ src/App.js
@@ -4,7 +4,6 @@ import Form from "./component/Form";
 function App() {
   return (
     <main>
-      <h1>Hello World</h1>
       <Form />
     </main>
   );
--- src/component/Form.js
+++ src/component/Form.js
@@ -9,30 +9,40 @@ import React from "react";
 // }
 
 export default function Form({ onAddActivity }) {
-  function handleSubmit(event /*name isForGoodWeather*/) {
+  function handleSubmit(event) {
     event.preventDefault();
+
     const newActivity = {
+      name: event.target.elements.activityName.value,
+      isGoodWeather: event.target.elements.isGoodWeather.checked,
+    };
+
+    onAddActivity(newActivity);
+
+    /*  const newActivity = {
       name: "activityName",
       isForGoodWeather: "weatherCheckbox.checked",
-    };
-  }
+    }; */
 
+    //onAddActivity(newActivity);
+    event.target.reset();
+  }
   return (
     <>
       <h1>Weather App</h1>
-      <form>
+      <form onSubmit={handleSubmit}>
         <div>
           <ul style={{ listStyle: "none" }}>
             <li>
               <label htmlFor="activityName">Activity Name:</label>
-              <input type="text" name="activityName"></input>
+              <input id="activityName" type="text" name="activityName"></input>
             </li>
             <li>
               <label htmlFor="weatherCheckbox">Weather Checkbox:</label>
               <input
                 type="checkbox"
                 name="weatherCheckbox"
-                checked={onAddActivity.isChecked}
+                //checked={isForGoodWeather}
               ></input>
             </li>
             <li>
