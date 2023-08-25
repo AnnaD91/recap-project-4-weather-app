f501e947585d809f58ec597dbb467071b4b235ee
Bonnie Baumeister <bonnie.hoika@gmx.de>
2023-08-25 12:58:14 +0200

    work on form

--- src/component/Form.js
+++ src/component/Form.js
@@ -1,22 +1,46 @@
 import React from "react";
 
-// Write a Form component which
-
-// contains a heading
-// has two input fields (name of activity, checkbox whether it is a good- or bad-weather activity) with appropriate labels and
-// a submit button;
-// it receives a prop called onAddActivity.
+// export default function EntryForm({ onAddEntry }) {
+// function handleSubmit(event) {
+//     event.preventDefault();
+//     const formData = new FormData(event.target);
+//     const data = Object.fromEntries(formData);
+//     onAddEntry(data);
+// }
 
 export default function Form({ onAddActivity }) {
-  //preventDefault()
+  function handleSubmit(event /*name isForGoodWeather*/) {
+    event.preventDefault();
+    const newActivity = {
+      name: "activityName",
+      isForGoodWeather: "weatherCheckbox.checked",
+    };
+  }
+
   return (
     <>
       <h1>Weather App</h1>
-      <label htmlFor="activityName">Activity Name:</label>
-      <input type="text" name="activityName"></input>
-      <label htmlFor="weatherCheckbox">Weather Checkbox:</label>
-      <input type="checkbox" name="weatherCheckbox"></input>
-      <button type="submit"> submit</button>
+      <form>
+        <div>
+          <ul style={{ listStyle: "none" }}>
+            <li>
+              <label htmlFor="activityName">Activity Name:</label>
+              <input type="text" name="activityName"></input>
+            </li>
+            <li>
+              <label htmlFor="weatherCheckbox">Weather Checkbox:</label>
+              <input
+                type="checkbox"
+                name="weatherCheckbox"
+                checked={onAddActivity.isChecked}
+              ></input>
+            </li>
+            <li>
+              <button type="submit"> submit</button>
+            </li>
+          </ul>
+        </div>
+      </form>
     </>
   );
 }
