6871abef09d9152315abe93d9cfa94467f9f8361
Anna Dick <anna_dick@outlook.de>
2023-08-25 16:28:25 +0200

    add local storage

--- src/App.js
+++ src/App.js
@@ -3,9 +3,12 @@ import Form from "./component/Form";
 import { useState } from "react";
 import { uid } from "uid";
 import List from "./component/List";
+import useLocalStorageState from "use-local-storage-state";
 
 function App() {
-  const [activities, setActivities] = useState([]);
+  const [activities, setActivities] = useLocalStorageState("activities", {
+    defaultValue: [],
+  });
   console.log(activities);
 
   function handleAddActivity(oneActivity) {
