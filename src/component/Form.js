a0a5ed4968a998a2d7e190d16de7e3d33819ed9d
Bonnie Baumeister <bonnie.hoika@gmx.de>
2023-08-25 11:13:56 +0200

    add form file

---
+++ src/component/Form.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+// Write a Form component which
+
+// contains a heading
+// has two input fields (name of activity, checkbox whether it is a good- or bad-weather activity) with appropriate labels and
+// a submit button;
+// it receives a prop called onAddActivity.
+
+<h1>heading</h1>;
