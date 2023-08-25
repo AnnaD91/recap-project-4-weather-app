8f5eb83f29f883b5fdb4b0e52f4d39c612f26500
Bonnie Baumeister <bonnie.hoika@gmx.de>
2023-08-25 11:37:11 +0200

    add first input

--- src/component/Form.js
+++ src/component/Form.js
@@ -7,4 +7,14 @@ import React from "react";
 // a submit button;
 // it receives a prop called onAddActivity.
 
-<h1>heading</h1>;
+export default function Form({ onAddActivity }) {
+  //preventDefault()
+  return (
+    <>
+      <h1>heading</h1>
+      <input type="text">name of activity</input>
+      <input type="checkbox">weather checkbox</input>
+      <button type="submit"> submit</button>
+    </>
+  );
+}
